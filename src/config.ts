e755a8f7c66c32476da3b3862443d8fac7fa9f34
Fabian Heim <fabian.heim@fs-students.de>
2026-05-09 17:55:05 +0200

    Update site

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const formConfig = {
-  WAITLIST_FORM_URL: '',
-  FEEDBACK_FORM_URL: '',
-  QUESTIONNAIRE_FORM_URL: '',
+  WAITLIST_FORM_URL: 'https://formspree.io/f/mzdojnrw',
+  FEEDBACK_FORM_URL: 'https://formspree.io/f/mvzlwerg',
+  QUESTIONNAIRE_FORM_URL: 'https://formspree.io/f/mwvyrgbv',
 }
 
 export type FormKey = keyof typeof formConfig
